fee21d8e5fdfb38413a05097692e7477080396de
Vasjen <vasjenm@gmail.com>
2023-03-02 01:43:59 +0400

    update 14

--- Easy/14/Solution.cs
+++ Easy/14/Solution.cs
@@ -8,28 +8,6 @@ If there is no common prefix, return an empty string "".
 */
   public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-     //  
-     //   int index=1;
-     //   bool Contains=true;
-     //   string begin=string.Empty;
-     // 
-     //   if (strs!=null && strs.Length>=1){
-     //     if (strs[0].Length>=1){
-     //   while(Contains){
-     //    begin=strs[0].Substring(0,index)?? "";
-     //    System.Console.WriteLine(begin);
-     //     foreach (var str in strs){
-     //       if (!str.StartsWith(begin)){
-     //         Contains=false;
-     //         return begin=begin.Remove(begin.Length-1);
-     //       }
-     //     }
-     //     index++;
-     //     
-     //     }
-     //   }
-     //   }
-     //   return begin;
           string common=string.Empty;
         if (strs!=null){
             int index =1;

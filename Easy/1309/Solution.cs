a0f28ad87a14fba763244520725b80c6a5e75a0e
Vasjen <vasjenm@gmail.com>
2023-03-05 20:57:48 +0400

    Add 1309

---
+++ Easy/1309/Solution.cs
@@ -0,0 +1,35 @@
+namespace LeetCode.Problem1309{
+//1309. Decrypt String from Alphabet to Integer Mapping
+//https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/
+/*
+You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+
+Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+Return the string formed after mapping.
+*/
+  public class Solution {
+   public string FreqAlphabets(string s) {
+    
+    Stack<char> StackS = new Stack<char>();
+
+    for (int i = s.Length - 1;i >= 0;i--)
+    {
+        var newChar = s.ElementAt(i);
+        if (newChar != '#'){
+           StackS.Push((char)(int)(s.ElementAt(i) + 48));
+        }
+        else{
+          var num = int.Parse(s.ElementAt(i - 2).ToString()+s.ElementAt(i - 1).ToString());
+          StackS.Push((char)(num + 96));
+          i = i - 2;
+        }
+    }
+   string answer = new string(StackS.ToArray());
+        return answer;
+        
+        
+    }
+
+}
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿
-var sol = new LeetCode.Problem2114.Solution();
-string[] sentence1 = new string[] {"please wait", "continue to fight", "continue to win"};
-var ans1 = sol.MostWordsFound(sentence1);
-System.Console.WriteLine(ans1);
+var sol = new LeetCode.Problem1309.Solution();
 
+string s1 = "10#11#12";
+var ans1 = sol.FreqAlphabets(s1);
+System.Console.WriteLine("S is {0}, ans is {1}",s1,ans1);
+
+string s2 = "1326#";
+var ans2 = sol.FreqAlphabets(s2);
+System.Console.WriteLine("S is {0}, ans is {1}",s2,ans2);
 
 
 

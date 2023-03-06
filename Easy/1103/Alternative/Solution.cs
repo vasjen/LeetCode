402e9d3d0b52f2f8edea3bfe9f89282ae7c9daaf
Vasjen <vasjenm@gmail.com>
2023-03-06 04:38:47 +0400

    1103 Alternative

---
+++ Easy/1103/Alternative/Solution.cs
@@ -0,0 +1,23 @@
+namespace LeetCode.Problem1103.ALternative{
+public class ALternative {
+    public int[] DistributeCandies(int candies, int num_people) {
+      
+        int counter = 0;
+        int index = 0;
+        int[] res = new int[num_people];
+        
+        while (candies - counter > 0)
+            {
+              index = counter % num_people;
+              counter++;
+              candies -= counter;
+              res[index] += counter;
+            }
+            
+        if (candies != 0)
+          res[counter % num_people] += candies;
+
+     return res;    
+    }
+}
+}

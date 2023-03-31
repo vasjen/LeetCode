8f936bf8f0435be80cc6860d58ada14446a39794
Vasjen <vasjenm@gmail.com>
2023-03-31 04:04:41 +0400

    Add 1576

---
+++ Easy/1576/Solution.cs
@@ -0,0 +1,62 @@
+ using System.Text;
+
+namespace LeetCode.Problem1576{
+//1576. Replace All ?'s to Avoid Consecutive Repeating Characters
+//https://leetcode.com/problems/replace-all-s-to-avoid-consecutive-repeating-characters/
+/*
+Given a string s containing only lowercase English letters and the '?' character, convert all the '?' characters 
+into lowercase letters such that the final string does not contain any consecutive repeating characters. 
+You cannot modify the non '?' characters.
+
+It is guaranteed that there are no consecutive repeating characters in the given string except for '?'.
+
+Return the final string after all the conversions (possibly zero) have been made. 
+If there is more than one solution, return any of them. It can be shown that an answer is always possible with the given constraints.
+*/
+  public class Solution {
+    
+   
+    public string ToLowerCase(string s) {
+        StringBuilder ans = new StringBuilder();
+        foreach (var item in s)
+        {
+            if (item >= 65 && item <= 90)
+                ans.Append((char)(item + 32));
+            else ans.Append(item);
+        }
+
+        return ans.ToString(); 
+    }
+    public string ModifyString(string s) {
+     StringBuilder ans = new StringBuilder();
+      var hz = ' ';
+      int val = 0;
+      for (int i = 0; i < s.Length; i++)
+      {
+        if (s[i] == '?')
+        {
+          char prev = i > 0 ? ans[i - 1] :  ' ';
+          char next = i < s.Length-1 ? s[i + 1] :  ' ';
+          hz = Convert.ToChar(97+val);
+          do
+          {
+            hz = Convert.ToChar(97+val);
+            val++;
+
+          }
+          while(hz == prev || hz == next);
+         
+              ans.Append(hz);
+              val = 0;
+           
+        }
+        else
+        
+          ans.Append(s[i]);
+        
+      }
+
+      return ans.ToString();
+    }
+  }
+} 
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿
-var sol = new LeetCode.Problem709.Solution();
-var s0 = "Hello";
-var ans0 = sol.ToLowerCase(s0);
+var sol = new LeetCode.Problem1576.Solution();
+var s0 = "?e?lo";
+var s1 = "?d?s?a??adad???a??a";
+var ans0 = sol.ModifyString(s0);
+var ans1 = sol.ModifyString(s1);
 System.Console.WriteLine("For string '{0}' lower is '{1}'",s0,ans0);
+System.Console.WriteLine("For string '{0}' lower is '{1}'",s1,ans1);

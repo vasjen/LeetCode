4a70e4cb7eaa941de56aa7cc33b92cd91a053042
Vasjen <vasjenm@gmail.com>
2023-04-01 00:09:37 +0400

    Add 1725

--- Program.cs
+++ Program.cs
@@ -1,13 +1,21 @@
 ﻿
-var sol = new LeetCode.Problem1207.Solution();
-var arr0 = new int[] {1,2,2,1,1,3};
-var ans0 = sol.UniqueOccurrences(arr0);
+var sol = new LeetCode.Problem1725.Solution();
+var arr0 = new int[][] {
+    new int[] {5,8},
+    new int[] {3,9},
+    new int[] {5,12},
+    new int[] {16,5},
+
+};
+var ans0 = sol.CountGoodRectangles(arr0);
 System.Console.WriteLine("Answer is {0}",ans0);
 
-var arr1 = new int[] {1,2};
-var ans1 = sol.UniqueOccurrences(arr1);
-System.Console.WriteLine("Answer is {0}",ans1);
+var arr1 = new int[][] {
+    new int[] {2,3},
+    new int[] {3,7},
+    new int[] {4,3},
+    new int[] {3,7},
 
-var arr2 = new int[] {-3,0,1,-3,1,1,1,-3,10,0};
-var ans2 = sol.UniqueOccurrences(arr2);
-System.Console.WriteLine("Answer is {0}",ans2);
+};
+var ans1 = sol.CountGoodRectangles(arr1);
+System.Console.WriteLine("Answer is {0}",ans1);

4b5838e5335a10bcf94d6bcaf388d1b8bece0c40
Vasjen <vasjenm@gmail.com>
2023-04-03 14:51:50 +0400

    Add 2057

---
+++ Easy/2057/Solution.cs
@@ -0,0 +1,20 @@
+namespace LeetCode.Problem2057{
+//https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+/*
+Given a 0-indexed integer array nums, return the smallest index i of nums such that i mod 10 == nums[i], or -1 
+if such index does not exist.
+
+x mod y denotes the remainder when x is divided by y.
+*/
+  public class Solution {
+   public int SmallestEqual(int[] nums) {
+        for (int i = 0; i < nums.Length; i++)
+        {
+          if (nums[i] == i % 10)
+            return i;
+        }
+
+        return -1;
+    }
+}
+} 
